a646aee07569f0442ec3b8806a589cd6a3c67c01
rokixy <zzubillzzu@126.com>
2021-05-19 13:31:04 +0800

    add 404 page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <div>Not Found 404</div>
+    <div>
+      <router-link to="/">返回首页</router-link>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NotFound",
+};
+</script>
