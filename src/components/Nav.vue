0b0b5dab3836be78e9a0c2bee87c4693d18aea9c
rokixy <zzubillzzu@126.com>
2021-05-19 21:52:48 +0800

    add style for nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       <Icon name="money" />
       记账
     </router-link>
-    <router-link to="/labels">
+    <router-link to="/labels" class="item">
       <Icon name="label" />
       标签
     </router-link>
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistic" />
       统计
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang="ts">
@@ -24,4 +24,21 @@ console.log(x);
 </script>
 
 <style lang="scss" scoped>
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+  flex-direction: row;
+  > .item {
+    padding: 2px 0;
+    width: 33.33333%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
+}
 </style>
