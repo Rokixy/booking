67ec02bb08b1c3f75f15f5b1b5b6a54326eeed98
rokixy <zzubillzzu@126.com>
2021-05-20 15:52:45 +0800

    refactoring html & css

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="tags">
+    <ul class="current">
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li>衣</li>
+      <li>食</li>
+      <li>住</li>
+      <li>行</li>
+      <li class="new">+</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Tags",
+};
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  font-size: 14px;
+  padding: 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column-reverse;
+  overflow: auto;
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+    margin-right: -5px;
+    > li {
+      background: #d9d9d9;
+      $h: 24px;
+      height: $h;
+      line-height: $h;
+      border-radius: $h/2;
+      padding: 0 16px;
+      margin-right: 12px;
+      margin-top: 4px;
+      &.new {
+        font-size: 24px;
+      }
+    }
+  }
+}
+</style>
