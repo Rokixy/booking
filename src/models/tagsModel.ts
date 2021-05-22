9a0a1bd527013a313eb86ef354219f85f895a4bb
rokixy <zzubillzzu@126.com>
2021-05-22 22:12:17 +0800

    could edit & delete tag in EditLabel page

--- src/models/tagsModel.ts
+++ src/models/tagsModel.ts
@@ -11,6 +11,8 @@ type TagsModel = {
     fetch: () => Tag[];
     save: () => void;
     create: (name: string) => 'success' | 'duplicated';
+    update: (id: string, name: string) => 'success' | 'duplicated' | 'not found';
+    remove: (id: string) => boolean
 }
 
 const tagsModel: TagsModel = {
@@ -30,6 +32,38 @@ const tagsModel: TagsModel = {
         this.data.push({ id: name, name: name });
         this.save();
         return 'success';
+    },
+    update(id, name) {
+        const idList = this.data.map(item => item.id);
+        if (idList.indexOf(id) >= 0) {
+            const names = this.data.map(item => item.name);
+            if (names.indexOf(name) >= 0) {
+                return 'duplicated';
+            } else {
+                const tag = this.data.filter(item => item.id === id)[0]
+                tag.name = name
+                this.save()
+                return 'success'
+            }
+        } else {
+            return 'not found'
+        }
+    },
+    remove(id) {
+        let index = -1;
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        if (index === -1) {
+            return false;
+        } else {
+            this.data.splice(index, 1);
+            this.save()
+            return true;
+        }
     }
 }
 
